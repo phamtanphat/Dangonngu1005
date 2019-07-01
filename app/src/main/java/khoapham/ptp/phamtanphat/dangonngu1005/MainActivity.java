2f5b3247bf36f98392e2d75b18afdda538a92f04
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-01 14:29:26 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/dangonngu1005/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/dangonngu1005/MainActivity.java
@@ -2,12 +2,37 @@ package khoapham.ptp.phamtanphat.dangonngu1005;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtTk,edtMk;
+    Button btnDangnhap;
+    TextView txtNoidung;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtMk = findViewById(R.id.edittextPassword);
+        edtTk = findViewById(R.id.edittextTaikhoan);
+        btnDangnhap = findViewById(R.id.buttonDangnhap);
+        txtNoidung = findViewById(R.id.textNoidung);
+
+        btnDangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String taikhoan = edtTk.getText().toString();
+                String matkhau = edtMk.getText().toString();
+
+                String tk = getResources().getString(R.string.text_taikhoan);
+                String mk = getResources().getString(R.string.text_matkhau);
+
+                txtNoidung.setText(tk +"  : " + taikhoan + "\n" + mk+"  : " + matkhau);
+            }
+        });
     }
 }
